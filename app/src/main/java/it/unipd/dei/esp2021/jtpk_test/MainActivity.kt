93010e177289cc136ebb5d711b2d0b9cbc46b379
il-nics <nicola.g316@gmail.com>
2025-05-08 18:27:45 +0200

    creazione progetto

---
+++ app/src/main/java/it/unipd/dei/esp2021/jtpk_test/MainActivity.kt
@@ -0,0 +1,243 @@
+package it.unipd.dei.esp2021.jtpk_test
+
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.rounded.AccountCircle
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import it.unipd.dei.esp2021.jtpk_test.ui.theme.Jtpk_testTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jtpk_testTheme {
+                Scaffold(){paddingValues ->
+                    Screen(modifier = Modifier.padding(paddingValues))
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(
+    modifier: Modifier = Modifier,
+    scrollBehaviour: TopAppBarScrollBehavior
+){
+    TopAppBar(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .clip(RoundedCornerShape(100.dp)),
+        scrollBehavior = scrollBehaviour,
+        colors =TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        windowInsets = WindowInsets(top=0.dp),
+        title = {
+            Text(
+                text = "Search",
+                color = MaterialTheme.colorScheme.onBackground.copy(0.7f),
+                //fontSize = 17.dp
+            )
+        },
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Rounded.Menu,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(start = 16.dp, end = 8.dp)
+                    .size(27.dp)
+            )
+        },
+        actions = {
+            Icon(
+                imageVector = Icons.Rounded.Notifications,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(30.dp)
+            )
+
+            Icon(
+                imageVector = Icons.Rounded.AccountCircle,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(start = 4.dp, end = 16.dp)
+                    .size(30.dp)
+            )
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier){
+    val scrollBehaviour = TopAppBarDefaults.enterAlwaysScrollBehavior(
+        state = rememberTopAppBarState()
+    )
+    Scaffold(
+        modifier = modifier
+            .nestedScroll(scrollBehaviour.nestedScrollConnection),
+        topBar = {
+            TopBar(
+                scrollBehaviour = scrollBehaviour
+            )
+        }
+    ) { paddingValues ->
+        /*ScreenContent(modifier = Modifier
+            //.padding(paddingValues)
+            .fillMaxSize()
+        )*/
+        Greetings(paddingValues)
+    }
+}
+
+@Composable
+fun Screen(modifier: Modifier = Modifier){
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            ScreenContent()
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier.fillMaxSize().background(Color.Red),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Welcome to the Basics Codelab")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ){
+            Text("Continue")
+        }
+    }
+}
+
+@Composable
+fun Greetings(paddingValues: PaddingValues,
+      names: List<String> = List(1000){"$it"}){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 4.dp),
+        contentPadding = paddingValues
+    ) {
+        items(items = names){ name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    Card(
+        //color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(12.dp)
+                    .animateContentSize(
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioLowBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )
+            )
+            {
+                Text(text = "Hello")
+                Text(text = name, style = MaterialTheme.typography.headlineMedium)
+                if (expanded){
+                    Text("Abbellaa!! Affantasticaaa!! Queto è il testo della tab che si apre " +
+                            "espandendo il contenuto e facendo una bouncinesss viejndneidendaisndeonsine")
+                }
+            }
+            IconButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Icon(
+                    imageVector =
+                        if (expanded) Icons.Filled.ExpandLess
+                        else Icons.Filled.ExpandMore,
+                    contentDescription =
+                        if (expanded) stringResource(R.string.show_less)
+                        else stringResource(R.string.show_less)
+                )
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyAppPreview() {
+    Jtpk_testTheme {
+        Screen(Modifier.fillMaxSize())
+    }
+}
